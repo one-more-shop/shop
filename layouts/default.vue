ddc90c743a90293476890a1d01ea6c8b29475012
juanmorschrott <juanmorschrott@outlook.es>
2023-01-07 12:06:05 +0100

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <v-app-bar elevation="4" prominent>
+      <v-form @submit.prevent="search">
+        <v-container>
+          <v-row>
+            <v-col cols="8" md="8">
+              <v-text-field
+                v-model="query"
+                label="Search"
+                append-icon="mdi-magnify"
+              />
+            </v-col>
+            <v-col cols="4" md="4">
+              <v-btn small color="blue" >Search</v-btn>
+            </v-col>
+          </v-row>
+        </v-container>
+      </v-form>
+    </v-app-bar>
+    <nuxt />
+  </div>
+</template>
+
+<style>
+/* Add your styles here */
+</style>
+
+<script>
+export default {
+  data() {
+    return {
+      query: "",
+    };
+  },
+  methods: {
+    search() {
+      // Add your search logic here
+      console.log(this.query);
+    },
+  },
+};
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="home">
+    <v-row>
+      <v-col>
+        <product-card />
+      </v-col>
+    </v-row>
+  </div>
+</template>
+
+<script>
+import ProductCard from '@/components/ProductCard.vue';
+
+export default {
+  name: "IndexPage",
+  components() {
+    ProductCard;
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  padding: 2em;
+}
+</style>
